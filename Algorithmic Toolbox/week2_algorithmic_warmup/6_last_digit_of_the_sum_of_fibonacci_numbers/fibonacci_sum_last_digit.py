cffff599ba73a7e6cce5a6dedfd607ec4920303e
Ahmedialm <ahmedabdulhamid2000@gmail.com>
2020-06-25 22:57:06 +0200

    fibonacci sum of last digit

---
+++ Algorithmic Toolbox/week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.py
@@ -0,0 +1,20 @@
+# Uses python3
+import sys
+
+def fibonacci_sum_naive(n):
+    if n <= 1:
+        return n
+
+    previous = 0
+    current  = 1
+    sum      = 1
+
+    for _ in range(n - 1):
+        previous, current = current, previous + current
+        sum += current
+
+    return sum % 10
+
+
+n = int(input())
+print(fibonacci_sum_naive(n))
