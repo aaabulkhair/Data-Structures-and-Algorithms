f32bf357b2b5d05cf23740bbe05bd99a86780aaa
Ahmedialm <ahmedabdulhamid2000@gmail.com>
2020-06-25 22:40:46 +0200

    fibonacci last digit

---
+++ Algorithmic Toolbox/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
@@ -0,0 +1,18 @@
+# Uses python3
+
+
+def get_fibonacci_last_digit_naive(n):
+    if n <= 1:
+        return n
+
+    previous = 0
+    current = 1
+
+    for _ in range(n - 1):
+        previous, current = current, previous + current
+
+    return current % 10
+
+
+n = int(input())
+print(get_fibonacci_last_digit_naive(n))
