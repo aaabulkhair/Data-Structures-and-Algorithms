2db8c98afe9a651e74c4fc9011a5712198ec02ce
Ahmedialm <ahmedabdulhamid2000@gmail.com>
2020-06-25 22:51:57 +0200

    least common multiple

---
+++ Algorithmic Toolbox/week2_algorithmic_warmup/4_least_common_multiple/lcm.py
@@ -0,0 +1,13 @@
+# Uses python3
+
+
+def lcm_naive(a, b):
+    for l in range(1, a * b + 1):
+        if l % a == 0 and l % b == 0:
+            return l
+
+    return a * b
+
+
+a, b = map(int, input().split())
+print(lcm_naive(a, b))
